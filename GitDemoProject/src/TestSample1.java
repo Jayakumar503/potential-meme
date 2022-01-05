14a87323bf66492dfb5f19f436133f66c0526dbe
Jayakumar503 <jayakumar1401cse@gmail.com>
2022-01-05 23:52:40 +0530

    first commit in eclipse..

---
+++ GitDemoProject/src/TestSample1.java
@@ -0,0 +1,15 @@
+
+public class TestSample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the TestSample1 class");
+		int c = 10;
+		if(c%2 == 0) {
+			System.out.println("C is even");
+		}else {
+			System.out.println("C is odd");
+		}
+	}
+
+}
